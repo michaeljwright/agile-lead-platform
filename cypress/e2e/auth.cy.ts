043f18dd3bfa7b29e6d5757078494ba680533831
Mike Wright <mike.filmworks@gmail.com>
2024-07-16 19:06:23 +0100

    Initial Cypress tests

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -3,7 +3,8 @@ describe('Test Authentication', () => {
     cy.login()
   })
 
-  it('Adds document to test_hello_world collection of Firestore', () => {
+  it('Check if logged in and can see full name of user on Account page', () => {
     cy.visit('http://localhost:3000/account')
+    cy.get('.c-kgWQHs').should('include.text','Mike Wright')
   })
 })
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,8 +1,12 @@
 describe('Test Homepage', () => {
-  it('Can I access the homepage and its elements', () => {
+  beforeEach(() => {
+    cy.logout()
+  })
+
+  it('Can I access the homepage and its elements but not other pages', () => {
     cy.visit('http://localhost:3000/')
     cy.get('.c-PJLV-jroWjL-alignItems-center > .c-fdnpOH > .c-kgWQHs').click()
     cy.get('.c-jcxtHY > .c-fdnpOH').click()
-    cy.get('.c-dgnNTJ').click()
+    cy.get('.c-dgnNTJ').contains('Oops! Please Sign In')
   })
 })
