fa6d876351345a5b7227b6a84313faa0272e19ca
Mike Wright <mike.filmworks@gmail.com>
2024-07-17 09:09:22 +0100

    Fixed linting issue

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -5,6 +5,6 @@ describe('Test Authentication', () => {
 
   it('Check if logged in and can see full name of user on Account page', () => {
     cy.visit('http://localhost:3000/account')
-    cy.get('.c-kgWQHs').should('include.text','Mike Wright')
+    cy.get('.c-kgWQHs').should('include.text', 'Mike Wright')
   })
 })
