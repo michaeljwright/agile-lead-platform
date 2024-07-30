5035df8a349df44e3f2c774c50bc068d53c63c7c
Mike Wright <mike.filmworks@gmail.com>
2024-07-30 19:56:17 +0100

    Completed first common component test

--- __tests__/components/common/buttonIcon.spec.tsx
+++ __tests__/components/common/buttonIcon.spec.tsx
@@ -1,6 +1,6 @@
 import { jest, describe, expect } from '@jest/globals'
 import { render, fireEvent } from '@testing-library/react'
-import { ButtonIcon } from '../components/common/button-icon'
+import { ButtonIcon } from '../../../components/common/button-icon'
 
 describe('ButtonIcon component', () => {
   it('should spread remaining props to Styles.Button component', () => {
@@ -53,21 +53,21 @@ describe('ButtonIcon component', () => {
     expect(onClickMock).toHaveBeenCalledTimes(1)
   })
 
-  // it('should not call onClick handler when button is disabled', () => {
-  //   const onClickMock = jest.fn()
+  it('should not call onClick handler when button is disabled', () => {
+    const onClickMock = jest.fn()
 
-  //   const { getByLabelText } = render(
-  //     <ButtonIcon
-  //       icon={{ name: 'test-icon', size: 16 }}
-  //       label="Test Button"
-  //       onClick={onClickMock}
-  //       disabled
-  //     />
-  //   )
+    const { getByLabelText } = render(
+      <ButtonIcon
+        icon={{ name: 'login', size: 16 }}
+        label="Test Button"
+        onClick={onClickMock}
+        disabled
+      />
+    )
 
-  //   const button = getByLabelText('Test Button');
-  //   button.click()
+    const button = getByLabelText('Test Button')
+    button.click()
 
-  //   expect(onClickMock).not.toHaveBeenCalled()
-  // })
+    expect(onClickMock).not.toHaveBeenCalled()
+  })
 })
